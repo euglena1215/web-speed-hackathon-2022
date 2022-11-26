137aaf19a8ea8bd9040f8ac8074caa2e08817b33
euglena1215 <teppest1215@gmail.com>
2022-11-27 01:02:26 +0900

    analyze production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,14 @@
 const { merge } = require('webpack-merge');
 const { commonClient, commonServer } = require('./webpack.common');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = [
   merge(commonClient, {
     mode: "production",
+    plugins: [
+      new BundleAnalyzerPlugin(),
+    ]
   }),
   merge(commonServer, {
     mode: "development", // TODO: production にするとエラーになったので後で直す
